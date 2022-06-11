771bc26d522f62a909ae4aaca544008fc9746db3
ddfesfes <janghs3071@gmail.com>
2022-06-11 13:14:33 +0900

    hello

---
+++ src/Component/portfolio/portfolio.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { fade } from 'svelte/transition';
+    import css from '../../lib/style.css';
+    import Switch from '../../lib/Switch.svelte';
+    import { t, locale, locales } from "../../lib/i18n";
+  
+    let dark = false;
+    
+    $: theme = dark ? 'dark' : 'lite';
+    let delay = 1;
+    
+    delay = delay * 1000;
+  
+    let userBrowserLang = navigator.language;
+    if (userBrowserLang.includes('-')) userBrowserLang = userBrowserLang.split('-')[0];
+  
+    if (!locales.includes(userBrowserLang)) userBrowserLang = 'en';
+  
+    $locale = userBrowserLang;
+</script>
+  
+<Switch bind:checked={ dark } key="bg"></Switch>
+<h1 in:fade="{{ duration: delay }}" class={ theme }>Hello World</h1>
